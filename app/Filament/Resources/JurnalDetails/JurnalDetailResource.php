b244caf8bffa2487cbbb159e1371ccbc2ad9df37
alviranazhwa <alviranazhwa2@gmail.com>
2026-04-26 12:52:24 +0700

    petshop

--- app/Filament/Resources/JurnalDetails/JurnalDetailResource.php
+++ app/Filament/Resources/JurnalDetails/JurnalDetailResource.php
@@ -23,11 +23,8 @@ class JurnalDetailResource extends Resource
     protected static ?string $modelLabel = 'Jurnal Umum';
     protected static ?string $pluralModelLabel = 'Jurnal Umum';
 
-<<<<<<< HEAD
     protected static bool $shouldRegisterNavigation = false;
 
-=======
->>>>>>> 6c7a423e16e534a652802d3bdb328469ecf71a99
     /**
      * IMPORTANT:
      * - Join ke tabel jurnal buat sorting default yang stabil (tanggal + nomor_jurnal)
